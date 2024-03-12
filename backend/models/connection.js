acd7d65ae014849d4311ef86897f245060763dd7
Swayam-Khare <swayamkhare78@gmail.com>
2024-03-12 15:29:57 +0530

    backend starter code added

---
+++ backend/models/connection.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const database = process.env.DATABASE;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+// ==== TO CONNECT TO LOCAL DATABASE ====
+// const sequelize = new Sequelize(database, user, password, {
+//   host: "localhost",
+//   dialect: "postgres",
+//   logging: false,
+// });
+
+// ==== TO CONNECT TO REMOTE DATABASE ====
+const sequelize = new Sequelize(
+  "postgres://invoice_db_v42o_user:QIAZyxp8hRZmEtlSTK17jYpAsNin4Az0@dpg-cnnvqr021fec73arfgvg-a.singapore-postgres.render.com/invoice_db_v42o?ssl=true"
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Create separate file for each model schema.
+// Add your model here:
+// Ex - db.<ModelName> = require('<model-path>')(sequilize, DataTypes);
+
+const check = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await db.sequelize.sync({ force: false });
+    console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+check();
+
+module.exports = { db, check };
