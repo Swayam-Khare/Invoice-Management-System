5653d1d40643cd2a2315d3816dc093188679ddb4
Sujit-BBSR <pattanaiksujit72@gmail.com>
2024-03-12 18:41:10 +0530

    Order model added

--- backend/models/invoiceModel.js
+++ backend/models/invoiceModel.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-      allowNull: false,
+      allowNull: false,  
     },
 
     due_date: {
