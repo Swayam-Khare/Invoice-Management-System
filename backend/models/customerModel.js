09b924327b2f583da3b595104666cc4d38cd5e5f
PPadihary <pratyush.padihary@gmail.com>
2024-03-12 20:12:32 +0530

    Committing addition of Customer model

---
+++ backend/models/customerModel.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+    const Customer = sequelize.define(
+      "Customer",
+      {
+        // Schema attributes
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          unique:true,
+          allowNull:false,
+          autoIncrement: true,
+          autoIncrement: { initialValue: 0 },
+        },
+        name: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: {
+              args: true,
+              msg: "Please enter a valid email address!",
+            },
+          },
+        },
+        contact: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          validate: {
+            args: [10, 10],
+            msg: "Contact must have exactly 10 digits",
+          },
+        },
+
+      {
+        // Other model options go here
+        {
+            // options
+            modelName: 'Customer',
+            tableName: 'customer',
+            timestamps: false
+          }
+      },
+    },
+    );
+  
+    return Customer;
+  };
+  
