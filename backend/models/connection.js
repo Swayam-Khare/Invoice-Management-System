2c6c08c5252197054478110f774bf6bbd4818dd7
Chandan Kumar <chandankumar995584@gmail.com>
2024-03-12 18:59:19 +0530

    added vendorModel

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -5,20 +5,21 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
 // ==== TO CONNECT TO LOCAL DATABASE ====
-// const sequelize = new Sequelize(database, user, password, {
-//   host: "localhost",
-//   dialect: "postgres",
-//   logging: false,
-// });
+const sequelize = new Sequelize(database, user, password, {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false,
+});
 
 // ==== TO CONNECT TO REMOTE DATABASE ====
-const sequelize = new Sequelize(
-  "postgres://invoice_db_v42o_user:QIAZyxp8hRZmEtlSTK17jYpAsNin4Az0@dpg-cnnvqr021fec73arfgvg-a.singapore-postgres.render.com/invoice_db_v42o?ssl=true"
-);
+// const sequelize = new Sequelize(
+//   "postgres://invoice_db_v42o_user:QIAZyxp8hRZmEtlSTK17jYpAsNin4Az0@dpg-cnnvqr021fec73arfgvg-a.singapore-postgres.render.com/invoice_db_v42o?ssl=true"
+// );
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+// db.Vendor = require('./vendorModel')(sequelize, DataTypes);
 
 // Create separate file for each model schema.
 // Add your model here:
@@ -28,7 +29,7 @@ const check = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    await db.sequelize.sync({ force: false });
+    await db.sequelize.sync({ force: true });
     console.log("All models were synchronized successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
