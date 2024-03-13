a2d554c86d6646af812abe27bd10719db05677ba
Chandan Kumar <chandankumar995584@gmail.com>
2024-03-13 13:41:58 +0530

    admin model created-Chandan

---
+++ backend/models/adminModel.js
@@ -0,0 +1,54 @@
+module.exports =(connectDB,DataTypes)=>{
+    const Admin = connectDB.define(
+        "Admin",
+        {
+          // Model attributes are defined here
+          id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            unique:true,
+            autoIncrement: true
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: {
+                args: true,
+                msg: "Please enter a valid email address!",
+              },
+            },
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: {
+                args: [8],
+                msg: "Password must be at least 8 characters long",
+              },
+            },
+          },
+        },
+        {
+          // Other model options go here
+          tableName: "admin",
+          timestamps: false,
+          hooks: {
+            beforeCreate: async (admin) => {
+              console.log("admin is ", admin);
+              const hashedPassword = await bcrypt.hash(admin.password, 10);
+              admin.password = hashedPassword;
+            },
+          },
+        }
+      );
+    
+      return Admin;
+}
