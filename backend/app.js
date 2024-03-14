a94037904ee65a2819e46c56526cdd50dfccccb6
vivek <kaal4study@gmail.com>
2024-03-14 11:30:08 +0530

    rearrange

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const globalErrorHandler = require("./utils/globalErrorHandler");
-const productRouter = require('./routes/productRoute')
+const productRouter = require("./routes/productRoute");
+const orderRouter = require("./routes/orderRoute");
+const authRouter = require("./routes/authRoute");
+const invoiceRouter = require("./routes/invoiceRoute");
+const vendorRoutes = require("./routes/vendorRoute");
+
 // USE MODULES HERE
 const app = express();
 app.use(cors());
 app.use(express.json());
-const orderRouter = require('./routes/orderRoute')
-const authRouter = require('./routes/authRoute');
 
 // ========================Invoice Router ====================
-const invoiceRouter = require ('./routes/invoiceRoute');
-
-
-const vendorRoutes = require('./routes/vendorRoute');
 
 // USE ROUTES HERE
-app.use("/api/v1",vendorRoutes );
-app.use('/api/v1/orders', orderRouter)
-app.use('/api/v1/auth', authRouter);
-app.use("/api/v1/invoices", invoiceRouter );
-app.use('/api/products', productRouter)
+app.use("/api/v1", vendorRoutes);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/invoices", invoiceRouter);
+app.use("/api/products", productRouter);
 
 // ERROR HANDLER MUST BE DEFINED LAST
 app.use(globalErrorHandler);
-
 
 module.exports = app;
