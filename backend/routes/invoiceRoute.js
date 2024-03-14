5753785609d4ef4f1c5d035b7d3cfc14e1c58e30
vivek <kaal4study@gmail.com>
2024-03-14 20:34:24 +0530

    protect route added in all controller

--- backend/routes/invoiceRoute.js
+++ backend/routes/invoiceRoute.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const invoiceController = require('./../controllers/invoiceController');
-
+const invoiceController = require("./../controllers/invoiceController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.route("/").get(authController.protect, invoiceController.getInvoices).post(invoiceController.addInvoice);
 
-router.route('/')
-    .get(invoiceController.getInvoices)
-    .post(invoiceController.addInvoice);
-
-router.route('/:invoice_id')
-    .get(invoiceController.getInvoice)
-    .patch(invoiceController.updateInvoice)
-    .delete(invoiceController.deleteInvoice);
+router
+  .route("/:invoice_id")
+  .get(authController.protect, invoiceController.getInvoice)
+  .patch(authController.protect, invoiceController.updateInvoice)
+  .delete(authController.protect, invoiceController.deleteInvoice);
 
 module.exports = router;
--- backend/routes/vendorRoute.js
+++ backend/routes/vendorRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const vendorController = require("../controllers/vendorController");
+const authController = require("../controllers/authController");
+
 const router = express.Router();
 
-router.route("/").
-post(vendorController.createVendor)
-.get(vendorController.getAllVendors);
+router.route("/").post(authController.protect, vendorController.createVendor).get(authController.protect, vendorController.getAllVendors);
 
-router.route('/:id').
-get(vendorController.getASpecificVendor)
-.delete(vendorController.deleteVendor)
-.patch(vendorController.updateVendor);
+router
+  .route("/:id")
+  .get(authController.protect, vendorController.getASpecificVendor)
+  .delete(authController.protect, vendorController.deleteVendor)
+  .patch(authController.protect, vendorController.updateVendor);
 module.exports = router;
