97f186d16d62ec50f34c18aabcfc05e9a5520a01
Chandan Kumar <chandankumar995584@gmail.com>
2024-03-15 16:14:08 +0530

    added admin controller -chandan

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,54 +1,65 @@
-module.exports =(connectDB,DataTypes)=>{
-    const Admin = connectDB.define(
-        "Admin",
-        {
-          // Model attributes are defined here
-          id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            unique:true,
-            autoIncrement: true
-          },
-          name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              isEmail: {
-                args: true,
-                msg: "Please enter a valid email address!",
-              },
-            },
-          },
-          password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: {
-                args: [8],
-                msg: "Password must be at least 8 characters long",
-              },
-            },
+module.exports = (connectDB, DataTypes) => {
+  const Admin = connectDB.define(
+    "Admin",
+    {
+      // Model attributes are defined here
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Please enter a valid email address!",
           },
         },
-        {
-          // Other model options go here
-          tableName: "admin",
-          timestamps: false,
-          hooks: {
-            beforeCreate: async (admin) => {
-              console.log("admin is ", admin);
-              const hashedPassword = await bcrypt.hash(admin.password, 10);
-              admin.password = hashedPassword;
-            },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [8],
+            msg: "Password must be at least 8 characters long",
           },
-        }
-      );
-    
-      return Admin;
-}
+        },
+      },
+      confirmPassword: {
+        type: DataTypes.VIRTUAL,
+        allowNull: false,
+        validate: {
+          isConfirmed(value) {
+            if (value !== this.password) {
+              throw new Error("Password and confirm password does not match");
+            }
+          },
+        },
+      }
+    },
+    {
+      // Other model options go here
+      tableName: "admin",
+      timestamps: false,
+      hooks: {
+        beforeCreate: async (admin) => {
+          console.log("admin is ", admin);
+          const hashedPassword = await bcrypt.hash(admin.password, 10);
+          admin.password = hashedPassword;
+        },
+      },
+    }
+  );
+
+  return Admin;
+};
