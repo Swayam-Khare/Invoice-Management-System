d94d7326873d2a6c11b283cf1d52ee472a9da58e
Chandan Kumar <chandankumar995584@gmail.com>
2024-03-16 15:50:26 +0530

    comment added

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -38,6 +38,7 @@ module.exports = (connectDB, DataTypes) => {
           },
         },
       },
+      // adding confirm field...
       confirmPassword: {
         type: DataTypes.VIRTUAL,
         allowNull: false,
