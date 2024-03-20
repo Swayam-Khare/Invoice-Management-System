1efc04ac1b91e96208aa1cae933c3ab94bc34110
Anant_Patel_288 <anantpatel288@gmail.com>
2024-03-20 11:50:25 +0530

    Revert "cust ctrl updated"
    This reverts commit eeb2cc1f4fb6d2a7cb1b96207eefe63a5e0b3a34.

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -7,32 +7,10 @@ const Address = db.Address;
 const VendorCustomer = db.VendorCustomer;
 const connectDB = db.connectDB;
 
-// -------------------------------------------- CREATE A CUSTOMER ---------------------------------------------
+// ------------- CREATE A CUSTOMER --------------
 
 exports.createCustomer = asyncErrorHandler(async (req, res, next) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    contact,
-    address_lane1,
-    address_lane2,
-    landmark,
-    pincode,
-    state,
-  } = req.body;
-
-  //restore customer if present in table
-  const count = await Customer.restore({
-    where: {
-      email,
-    },
-  });
-
-  //create new customer if no record is restored
-  if (!count) {
-    const customer = await Customer.create(
-      {
+    const {
         firstName,
         lastName,
         email,
@@ -44,6 +22,7 @@ exports.createCustomer = asyncErrorHandler(async (req, res, next) => {
         state,
         role,
     } = req.body;
+<<<<<<< HEAD
 
 
     let customer = await Customer.findOne({ where: { email }, paranoid: false });
@@ -166,79 +145,113 @@ exports.createCustomer = asyncErrorHandler(async (req, res, next) => {
         });
     }
 });
+=======
+>>>>>>> parent of eeb2cc1 (cust ctrl updated)
 
-    //create new record for vendorcustomer table
-    const vendorCustomer = await VendorCustomer.create({
-      VendorId: req.vendor.id,
-      CustomerId: customer.id,
+    //restore customer if present in table
+    const count = await Customer.restore({
+        where: {
+            email
+        }
     });
 
-    res.status(201).json({
-      status: "Success",
-      data: {
-        customer,
-        Vendor_Customer_Details: vendorCustomer,
-      },
-    });
-  } else {
-    //find restored customer
-    let customer = await Customer.findOne({ where: { email } });
+    //create new customer if no record restored
+    if (count === 0) {
+        const customer = await Customer.create(
+            {
+                firstName,
+                lastName,
+                email,
+                contact,
+                Address_Details: {
+                    address_lane1,
+                    address_lane2,
+                    landmark,
+                    pincode,
+                    state,
+                    contact,
+                    role,
+                },
+            },
+            {
+                include: [db.customerAddress],
+            }
+        );
 
-    //restore address of already restored customer
-    await Address.restore({
-      where: {
-        role: "customer",
-        roleId: customer.id,
-      },
-    });
+        //create new record for VendorCustomer table
+        const vendorCustomer = await VendorCustomer.create({
+            VendorId: req.vendor.id,
+            CustomerId: customer.id,
+        });
 
-    //find restored address
-    customer = await Customer.findOne({
-      include: [
-        {
-          model: Address,
-          as: "Address_Details",
-        },
-      ],
-      where: { email },
-    });
+        res.status(201).json({
+            status: "Success",
+            data: {
+                customer,
+                "Vendor/Customer details": vendorCustomer 
+            },
+        });
+    } else {
+        //find restored customer
+        const customer = await Customer.findOne({ where: { email } });
 
-    //restore vendorvustomer table record
-    await VendorCustomer.restore({
-      where: {
-        VendorId: req.vendor.id,
-        CustomerId: customer.id,
-      },
-    });
+        //restore address of already restored customer
+        await Address.restore({
+            where: {
+                role: "customer",
+                roleId: customer.id
+            }
+        });
 
-    //find restored VendorCustomer records
-    const vendorCustomer = await VendorCustomer.findOne({
-      where: {
-        VendorId: req.vendor.id,
-        CustomerId: customer.id,
-      },
-    });
+        //restore VendorCustomer table records
+        await VendorCustomer.restore({
+            where: {
+                VendorId: req.vendor.id,
+                CustomerId: customer.id
+            }
+        });
 
-    res.status(201).json({
-      status: "Success",
-      data: {
-        customer,
-        Vendor_Customer_Details: vendorCustomer,
-      },
-    });
-  }
+        //find restored address
+        const address = await Address.findOne({ where: { roleId: customer.id } });
+
+        //find restored VendorCustomer records
+        const vendorCustomer = await VendorCustomer.findOne({where: {VendorId: req.vendor.id, CustomerId: customer.id}});
+
+        res.status(201).json({
+            status: "Success",
+            data: {
+                customer,
+                "Address Details": address,
+                "Vendor/Customer details": vendorCustomer
+            },
+        });
+    }
 });
 
-// -------------------------------------------- GET ALL CUSTOMERS ---------------------------------------------
+// ------------- GET ALL CUSTOMERS --------------
 
 exports.getAllCustomers = asyncErrorHandler(async (req, res, next) => {
-  // FETCHING ALL THE CUSTOMER ID CORRESPONDING TO VENDOR ID
-  let vendorCustomers = await VendorCustomer.findAll({
-    where: {
-      VendorId: req.vendor.id,
-    },
-  });
+    const customers = await Customer.findAll({
+        // paranoid: false,
+        include: [
+            {
+                model: Address,
+                // paranoid: false,
+                as: "Address_Details",
+                attributes: [
+                    "address_lane1",
+                    "address_lane2",
+                    "landmark",
+                    "pincode",
+                    "state",
+                    "contact"
+                ],
+            },
+        ],
+        attributes: ["id", "firstName", "lastName", "email", "contact"],
+    });
 
+<<<<<<< HEAD
   // EXTRACTING CUSTOMER ID FROM VENDORCUSTOMERS ARRAY AND SAVING IT INTO CUSTOMERID ARRAY
   const customerIds = vendorCustomers.map(
     (vendorCustomer) => vendorCustomer.CustomerId
@@ -264,132 +277,152 @@ exports.getAllCustomers = asyncErrorHandler(async (req, res, next) => {
       vendorCustomers,
     },
   });
+=======
+    res.status(200).json({
+        status: "Success",
+        count: customers.length,
+        data: {
+            customers,
+        },
+    });
+});
+>>>>>>> parent of eeb2cc1 (cust ctrl updated)
 
-// ------------------------------------------- GET CUSTOMER BY ID ---------------------------------------------
+// ------------- GET CUSTOMER BY ID --------------
 
 exports.getCustomer = asyncErrorHandler(async (req, res, next) => {
-  const customer = await Customer.findByPk(req.params.id, {
-    include: [
-      {
-        model: Address,
-        as: "Address_Details",
-      },
-    ],
-  });
+    const customer = await Customer.findOne({
+        include: [
+            {
+                model: Address,
+                as: "Address Details",
+                attributes: [
+                    "address_lane1",
+                    "address_lane2",
+                    "landmark",
+                    "pincode",
+                    "state",
+                    "contact"
+                ],
+            },
+        ],
+        attributes: ["id", "firstName", "lastName", "email", "contact"],
+        where: {
+            id: req.params.id,
+        },
+    });
 
-  if (!req.params.id) {
-    return next(new CustomError("Please provide id in parameters!", 400));
-  }
+    if (!req.params.id || !customer) {
+        if (!req.params.id) {
+            return next(new CustomError("Please provide id in parameters!", 400));
+        }
+        if (!customer) {
+            return next(new CustomError(`Customer with the id(${req.params.id}) does not exist!`, 400));
+        }
+    }
 
-  if (!customer) {
-    return next(
-      new CustomError(
-        `Customer with the id(${req.params.id}) does not exist!`,
-        400
-      )
-    );
-  }
-
-  res.status(200).json({
-    status: "Success",
-    data: {
-      customer,
-    },
-  });
+    res.status(200).json({
+        status: "Success",
+        data: {
+            customer,
+        },
+    });
 });
 
-// --------------------------------------------- DELETE CUSTOMER ----------------------------------------------
+// ------------- DELETE CUSTOMER --------------
 
 exports.deleteCustomer = asyncErrorHandler(async (req, res, next) => {
-  const customer = await Customer.findByPk(req.params.id);
+    const customer = await Customer.findByPk(req.params.id)
 
-  if (!req.params.id) {
-    return next(new CustomError("Please provide id in parameters!", 400));
-  }
+    if (!req.params.id || !customer) {
+        if (!req.params.id) {
+            return next(new CustomError("Please provide id in parameters!", 400));
+        }
+        if (!customer) {
+            return next(new CustomError(`Customer with the id(${req.params.id}) does not exist!`, 400));
+        }
+    }
 
-  if (!customer) {
-    return next(
-      new CustomError(
-        `Customer with the id(${req.params.id}) does not exist!`,
-        400
-      )
-    );
-  }
+    await Customer.destroy({ where: { id: req.params.id } });
+    await Address.destroy({ where: { role: "customer", roleId: req.params.id } });
 
-  await Customer.destroy({ where: { id: req.params.id } });
-  await Address.destroy({ where: { role: "customer", roleId: req.params.id } });
-  await VendorCustomer.destroy({
-    where: { VendorId: req.vendor.id, CustomerId: req.params.id },
-  });
-
-  res.status(200).json({
-    status: "Success",
-    message: "Customer has been deleted successfully",
-  });
+    await VendorCustomer.destroy({where: {VendorId: req.vendor.id, CustomerId: req.params.id}});
+    res.status(200).json({
+        status: "Success",
+        message: "Customer has been deleted successfully",
+    });
 });
 
-// --------------------------------------------- UPDATE CUSTOMER ----------------------------------------------
+// ------------- UPDATE CUSTOMER --------------
 
 exports.updateCustomer = asyncErrorHandler(async (req, res, next) => {
-  const customer = await Customer.findByPk(req.params.id);
+    const customer = await Customer.findByPk(req.params.id)
 
-  if (!req.params.id) {
-    return next(new CustomError("Please provide id in parameters!", 400));
-  }
+    if (!req.params.id || !customer) {
+        if (!req.params.id) {
+            return next(new CustomError("Please provide id in parameters!", 400));
+        }
+        if (!customer) {
+            return next(new CustomError(`Customer with the id(${req.params.id}) does not exist!`, 400));
+        }
+    }
 
-  if (!customer) {
-    return next(
-      new CustomError(
-        `Customer with the id(${req.params.id}) does not exist!`,
-        400
-      )
+    const {
+        firstName,
+        lastName,
+        email,
+        contact,
+        address_lane1,
+        address_lane2,
+        landmark,
+        pincode,
+        state,
+        role,
+    } = req.body;
+
+    if (role) {
+        const error = new CustomError(
+            "You can't update role using this end point!",
+            400
+        );
+        return next(error);
+    };
+
+    const updateCustomer = await Customer.update(
+        { firstName, lastName, email, contact },
+        { where: { id: req.params.id } }
     );
-  }
 
-  const {
-    firstName,
-    lastName,
-    email,
-    contact,
-    address_lane1,
-    address_lane2,
-    landmark,
-    pincode,
-    state,
-    role,
-  } = req.body;
-
-  if (role) {
-    const error = new CustomError(
-      "You can't update role using this end point!",
-      400
+    const updateCustomerAddress = await Address.update(
+        { address_lane1, address_lane2, landmark, pincode, state, contact },
+        { where: { role: "customer", roleId: req.params.id } }
     );
-    return next(error);
-  }
 
-  const updateCustomer = await Customer.update(
-    { firstName, lastName, email, contact },
-    { where: { id: req.params.id } }
-  );
+    const updatedCustomer = await Customer.findByPk(req.params.id, {
+        include: [
+            {
+                model: Address,
+                as: "Address Details",
+                attributes: [
+                    "address_lane1",
+                    "address_lane2",
+                    "landmark",
+                    "pincode",
+                    "state",
+                    "contact"
+                ],
+            },
+        ],
+        attributes: ["id", "firstName", "lastName", "email", "contact"],
+        where: {
+            id: req.params.id,
+        },
+    });
 
-  const updateCustomerAddress = await Address.update(
-    { address_lane1, address_lane2, landmark, pincode, state, contact },
-    { where: { role: "customer", roleId: req.params.id } }
-  );
-
-  const updatedCustomer = await Customer.findByPk(req.params.id, {
-    include: [
-      {
-        model: Address,
-        as: "Address_Details",
-      },
-    ],
-  });
-
-  res.status(200).json({
-    status: "Success",
-    data: {
-      updatedCustomer,
-    },
-  });
+    res.status(200).json({
+        status: "Success",
+        data: {
+            updatedCustomer,
+        },
+    });
 });
