f6494610e515521fb2a269adb899d3d51e132ae0
Chandan Kumar <chandankumar995584@gmail.com>
2024-03-28 12:02:32 +0530

    added

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -88,11 +88,10 @@ const check = async () => {
   try {
     await connectDB.authenticate();
     console.log("Connection has been established successfully.");
-    await db.connectDB.sync({ force: false });
+    await db.connectDB.sync({ force: false, alter:true });
     console.log("All models were synchronized successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
-
 module.exports = { db, check };
