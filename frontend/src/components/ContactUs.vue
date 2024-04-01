b7c91d942dc5bcd09c1c9f338cd08679050e72fb
PPadihary <pratyush.padihary@gmail.com>
2024-04-01 19:13:00 +0530

    Added the ContactUS component

--- frontend/src/components/ContactUs.vue
+++ frontend/src/components/ContactUs.vue
@@ -1,133 +1,141 @@
 <template>
-    <div id="hello">
-    <v-form>
-      <v-row justify="center" class="mb-2"> <!-- Reduce margin bottom -->
-        <v-col cols="12" class="text-center">
-          <v-card-title class="mt-3">
-            <h3 style="color: #112d4e">Send us a message and we will get in touch!</h3>
-          </v-card-title>
-        </v-col>
-      </v-row>
-      <v-card-text>
-        <v-form class="px-3" ref="form" @submit.prevent="submitForm">
-          <v-row justify="center">
-            <v-col cols="12" md="3" class="pb-0 pb-md-2"> <!-- Reduce padding bottom -->
-              <v-text-field
-                label="First Name"
-                v-model="firstName"
-                :rules="[alphabetOnlyRule]"
-                variant="outlined"
-                color="#112d4e"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3" class="pb-0 pb-md-2"> <!-- Reduce padding top -->
-              <v-text-field
-                label="Last Name"
-                v-model="lastName"
-                :rules="[alphabetOnlyRule]"
-                variant="outlined"
-                color="#112d4e"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row justify="center">
-            <v-col cols="12" md="6" class="pb-0 pb-md-2"> <!-- Reduce padding bottom -->
-              <v-text-field
-                label="Email"
-                :rules="[emailRule]"
-                v-model="email"
-                variant="outlined"
-                color="#112d4e"
-                class="mt-1"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row justify="center">
-            <v-col cols="12" md="6" class="pb-0 pb-md-2"> <!-- Reduce padding bottom -->
-              <v-text-field
-                label="Contact No."
-                :rules="contactNoRules"
-                v-model="contactNo"
-                variant="outlined"
-                color="#112d4e"
-                class="mt-1"                
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row justify="center">
-          <v-col cols="12" md="6" class="pb-0 pb-md-2">
-            <v-textarea
-              label="Leave us a message"
-              v-model="messageUs"
-              variant="outlined"
-              color="#112d4e"
-              class="mt-1"
-              rows="4"
-            ></v-textarea>
-          </v-col>
-        </v-row>
-          <v-row justify="center">
-            <v-col cols="12" md="3" class="pb-0 pb-md-2"> <!-- Reduce padding bottom -->
-              <v-btn class="mt-1 txt-button" color="#112d4e" @click="validate" block>Send Message</v-btn>        
-            </v-col>
-          </v-row>    
-        </v-form>
-      </v-card-text>
-    </v-form>
-    </div>
-  </template>
-  
-  
-  <script>
-  export default {
-    data() {
-      return {
-        firstName: '',
-        lastName: '',
-        email: '',
-        contactNo: '',
-        messageUs: '',
-        alphabetOnlyRule: (v) => /^[A-Za-z\s]*$/.test(v) || 'Alphabets only.',
-        emailRule: (v) => /.+@.+\..+/.test(v) || 'Invalid email address.'
-      }
-    },
-    props: {
-      styling: String
-    },
-    computed: {
-      contactNoRules() {
-        return [
-          (v) => !!v || 'Contact number is required.',
-          (v) => (v && /^\d+$/.test(v)) || 'Contact number must contain only digits.',
-          (v) => (v && v.length >= 10) || 'Contact number must be at least 10 digits.'
-        ]
-      }
-    },
-    methods: {
-      submitForm() {
-        if (this.$refs.form.validate()) {
-          // Process the message data here
-          console.log('Details submitted!')
-          // Reset form fields
-          this.resetForm()
-        }
-      },
-      validate() {
-        this.$refs.form.validate()
-      },
-      resetForm() {
-        this.firstName = ''
-        this.lastName = ''
-        this.email = ''
-        this.messageUs = ''
-      }
-    }
+  <div id="formContainer">
+    <v-sheet class="mx-auto mt-8" height="auto" max-width="1000" width="100%">
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card v-bind="props" :elevation="isHovering ? 24 : 6" class="mx-auto">
+          <v-form>
+            <v-row justify="center" class="mb-2">
+              <v-col cols="12" class="text-center">
+                <v-card-title class="mt-3">
+                  <h3 style="color: #112d4e" :class="{ 'text-large': $vuetify.display.mdAndUp, 'text-normal': $vuetify.display.smAndDown }">Send us a message and we will get in touch!</h3>
+                </v-card-title>
+
+              </v-col>
+            </v-row>
+            <v-card-text>
+              <v-form class="px-3" ref="form" @submit.prevent="submitForm">
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-text-field
+                      label="First Name"
+                      v-model="firstName"
+                      :rules="[alphabetOnlyRule]"
+                      variant="outlined"
+                      color="#112d4e"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-text-field
+                      label="Last Name"
+                      v-model="lastName"
+                      :rules="[alphabetOnlyRule]"
+                      variant="outlined"
+                      color="#112d4e"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Email"
+                      :rules="[emailRule]"
+                      v-model="email"
+                      variant="outlined"
+                      color="#112d4e"
+                      class="mt-1"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Contact No."
+                      :rules="contactNoRules"
+                      v-model="contactNo"
+                      variant="outlined"
+                      color="#112d4e"
+                      class="mt-1"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-textarea
+                      label="Leave us a message"
+                      v-model="messageUs"
+                      variant="outlined"
+                      color="#112d4e"
+                      class="mt-1"
+                      rows="4"
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-row justify="center">
+                  <v-col cols="12" sm="6" md="4">
+                    <v-btn class="mt-1 txt-button" color="#112d4e" @click="validate" block>Send Message</v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-form>
+        </v-card>
+      </v-hover>
+    </v-sheet>
+  </div>
+</template>
+
+
+
+
+<script setup>
+import { ref, computed } from 'vue'
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const contactNo = ref('')
+const messageUs = ref('')
+const alphabetOnlyRule = (v) => /^[A-Za-z\s]*$/.test(v) || 'Alphabets only.'
+const emailRule = (v) => /.+@.+\..+/.test(v) || 'Invalid email address.'
+
+const contactNoRules = computed(() => [
+  (v) => !!v || 'Contact number is required.',
+  (v) => (v && /^\d+$/.test(v)) || 'Contact number must contain only digits.',
+  (v) => (v && v.length >= 10) || 'Contact number must be at least 10 digits.'
+])
+const form = ref(null) // If you need a ref to the form for validation
+function submitForm() {
+  if (form.value.validate()) {
+    console.log('Your details have been submitted!')
+    resetForm()
   }
-  </script>
-  <style scoped>
-  .title {
-    color: #112d4e;
-    font-size: 1.5rem;
-    margin: 0;
-  }
-  </style>
+}
+function validate() {
+  form.value.validate()
+}
+function resetForm() {
+  firstName.value = ''
+  lastName.value = ''
+  email.value = ''
+  contactNo.value = ''
+  messageUs.value = ''
+}
+</script>
+
+<style>
+.rounded-sheet
+{
+  border-radius: 100px;
+}
+.white-space-normal {
+  white-space: normal;
+}
+
+.text-large {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.text-normal {
+  white-space: normal;
+}
+</style>
