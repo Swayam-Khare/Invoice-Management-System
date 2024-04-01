710c1dfb18c9e21c88259f59a37e41e178bf7a90
Anant Patel <72664416+anantpatel2002@users.noreply.github.com>
2024-04-01 16:58:22 +0530

    Update About us

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -53,7 +53,7 @@
     <hr class="mx-auto my-12" style="border: none; border-top: 7px dotted #112d4eaa; width: 15%" />
     <div id="team">
       <p class="text-h4 text-center font-weight-bold mb-5">Our Team</p>
-      <v-row class="w-100 ma-0 ma-xs-6 mx-md-16 my-md-11">
+      <v-row class="ma-0 ma-xs-6 mx-md-16 my-md-11">
         <v-col v-for="member in teamMembers" :key="member.id" cols="12" sm="6" md="4">
           <v-card color="justify-center align-center d-flex " flat>
             <div class="user-container">
