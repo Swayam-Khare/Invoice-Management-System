e1a12e0c0f01b1e397becc735436baaab53d23a5
Anant_Patel_288 <anantpatel288@gmail.com>
2024-04-02 18:07:55 +0530

    Admin page created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
-
 <template>
-  <Home />
+  <!-- <Home /> -->
+  <Admin />
 </template>
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import Admin from './views/Admin.vue'
 </script>
 
 <style></style>
