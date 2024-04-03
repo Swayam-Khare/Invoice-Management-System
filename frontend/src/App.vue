345da03387ce059e79b3cb3f70e42dac2df7bee9
Sumit Sharma <[valid-email]>
2024-04-04 00:01:03 +0530

    VendorStartupCode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <!-- <Home /> -->
-  <Admin />
+  <!-- <Admin /> -->
+  <Vendor />
 </template>
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 // import Home from './views/Home.vue'
-import Admin from './views/Admin.vue'
+// import Admin from './views/Admin.vue'
+import Vendor from './views/Vendor.vue'
 </script>
 
 <style></style>
