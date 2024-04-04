81f18fbf2e4c7c522c784043fccb643e9fdaa27c
Chandan Kumar <chandankumar995584@gmail.com>
2024-04-04 10:28:22 +0530

    working on product page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <Home />
+  <!-- <Home /> -->
 
   <!-- <Admin /> -->
-  <Vendor />
-
+  <!-- <Vendor /> -->
+<Product/>
 </template>
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 // import Admin from './views/Admin.vue'
 // import Vendor from './views/Vendor.vue'
 // import Customer from './components/Customer.vue'
+import Product from './components/Product.vue';
 
 </script>
 
