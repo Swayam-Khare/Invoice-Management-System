599b07e0f0460f66a850c8852f1766b10a639815
Chandan Kumar <chandankumar995584@gmail.com>
2024-04-06 01:37:39 +0530

    testing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App.vue'
+import Admin from '@/views/Admin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'app',
       component: App
+    }, {
+      path: '/admin',
+      name: 'admin',
+      component: Admin
     }
   ]
 })
--- frontend/src/stores/admin.js
+++ frontend/src/stores/admin.js
@@ -4,15 +4,18 @@ import axios from 'axios';
 
 export const useAdminStore = defineStore('adminStore', () => {
     const adminArr = ref([]);
+    let token = ref(null);
 
     async function loginAdmin(formData) {
         console.log('in store',formData)
         // const {email,password} = formData;
         const config = { headers: { "Content-Type": "application/json" }, withCredentials: true};
         const res = await axios.post('http://localhost:3500/api/v1/auth/login/admin', formData, config);
-        console.log(res);
+        // console.log(res);
+        token = res.data.token
+        console.log(token)
 
     }
-    return {adminArr,loginAdmin};
+    return {adminArr,loginAdmin,token};
 })
 
