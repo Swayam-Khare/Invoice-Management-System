8a819154528ef38ccd923fc169329091ca87fad9
PPadihary <pratyush.padihary@gmail.com>
2024-04-05 10:54:07 +0530

    None

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <!-- <Home /> -->
+  <Home />
 
   <!-- <Admin /> -->
-  <Vendor />
+  <!-- <Vendor /> -->
 
 </template>
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-// import Home from './views/Home.vue'
+import Home from './views/Home.vue'
 // import Admin from './views/Admin.vue'
-import Vendor from './views/Vendor.vue'
+// import Vendor from './views/Vendor.vue'
 // import Customer from './components/Customer.vue'
 
 </script>
