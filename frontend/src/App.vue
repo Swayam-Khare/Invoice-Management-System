430720789a10792fae9a841c618f33b4fa19d342
vivek <kaal4study@gmail.com>
2024-04-09 12:38:56 +0530

    update-vendor ui change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,17 +10,6 @@
   <!-- <SelectCustomer /> -->
 </template>
 
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Home from './views/Home.vue'
-import Admin from './views/Admin.vue'
-import Vendor from './views/Vendor.vue'
-import { onMounted } from 'vue'
-import router from './router'
-// import Customer from './components/Customer.vue'
-// import Product from './components/Product.vue'
-import CreateInvoice from './components/CreateInvoice.vue'
-import SelectCustomer from './components/SelectCustomer.vue'
-</script>
+<script setup></script>
 
 <style></style>
