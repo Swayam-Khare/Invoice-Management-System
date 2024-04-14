acd651e92f909dd66f7a56020dbb65b66e353e02
vivek <kaal4study@gmail.com>
2024-04-14 11:03:01 +0530

    ef

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <RouterView />
   <!-- <Admin /> -->
-  <Vendor />
+  <!-- <Vendor /> -->
   <!-- <Product /> -->
   <!-- <CreateInvoice /> -->
   <!-- <SelectCustomer /> -->
@@ -9,9 +9,9 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 // import Admin from './views/Admin.vue'
-import Vendor from './views/Vendor.vue'
+// import Vendor from './views/Vendor.vue'
 // import { onMounted } from 'vue';
 // import router from './router'
 // import Customer from './components/Customer.vue'
