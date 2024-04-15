e40788e0fa2e45a1a6b392d556d253fb76e98959
vivek <kaal4study@gmail.com>
2024-04-15 14:25:41 +0530

    pincode

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
 import './assets/main.css'
-import 'vue3-toastify/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // This is the css file for toast
-import 'vue3-toastify/dist/index.css'; 
+import 'vue3-toastify/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -33,6 +32,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-
 
 app.mount('#app')
